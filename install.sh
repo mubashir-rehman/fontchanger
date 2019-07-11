d36c3c72c9cc49a4c9427c39e60ae30f7b1fbc63
JohnFawkes <33303104+JohnFawkes@users.noreply.github.com>
2019-07-11 05:54:34 -0400

    ugh
    Signed-off-by: JohnFawkes <33303104+JohnFawkes@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -132,8 +132,7 @@ on_install() {
   # The following is the default implementation: extract $ZIPFILE/system to $MODPATH
   # Extend/change the logic to whatever you want
   ui_print "- Extracting module files"
-#  unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
-  unzip -o "$ZIPFILE" "$MODID/*" -d ${MODPATH%/*}/ >&2
+  unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
   chmod 0755 $TMPDIR/busybox-$ARCH32
   ui_print " [-] Checking For Internet Connection... [-] "
 if $BOOTMODE; then
