724dbc3adbd9a1d86baa6aabb574b6f24d7974ed
JohnFawkes <33303104+JohnFawkes@users.noreply.github.com>
2019-07-12 01:12:10 -0400

    update done
    Signed-off-by: JohnFawkes <33303104+JohnFawkes@users.noreply.github.com>

--- install-latest.sh
+++ install-latest.sh
@@ -1,11 +1,10 @@
 #!/system/bin/sh
-trap 'e=$?; echo; exit $e' EXIT
 SDCARD=/storage/emulated/0
 FCDIR=$SDCARD/Fontchanger
 MODID=Fontchanger
 
 # Detect root
-_name=$(basename ${0:-})
+_name=$(basename $0)
 ls /data >/dev/null 2>&1 || { echo "$MODID needs to run as root!"; echo "type 'su' then '$_name'"; quit 1; }
 
 if ! which busybox > /dev/null; then
@@ -26,45 +25,44 @@ elif [ -f /data/magisk/util_functions.sh ]; then
 else
   echo "! Can't find magisk util_functions! Aborting!"; exit 1
 fi
-set -euo pipefail
+
 imageless_magisk() {
   [ $MAGISK_VER_CODE -gt 18100 ]
   return $?
 }
 
 set_perm() {
-  chown ${2:-}:${3:-} ${1:-} || return 1
-  chmod ${4:-} ${1:-} || return 1
-  CON=${5:-}
+  chown $2:$3 $1 || return 1
+  chmod $4 $1 || return 1
+  CON=$5
   [ -z $CON ] && CON=u:object_r:system_file:s0
-  chcon $CON ${1:-} || return 1
+  chcon $CON $1 || return 1
 }
 
 set_perm_recursive() {
-  find ${1:-} -type d 2>/dev/null | while read dir; do
-    set_perm $dir ${2:-} ${3:-} ${4:-} ${6:-}
+  find $1 -type d 2>/dev/null | while read dir; do
+    set_perm $dir $2 $3 $4 $6
   done
-  find ${1:-} -type f -o -type l 2>/dev/null | while read file; do
-    set_perm $file ${2:-} ${3:-} ${5:-} ${6:-}
+  find $1 -type f -o -type l 2>/dev/null | while read file; do
+    set_perm $file $2 $3 $5 $6
   done
 }
 
 get_file_value() {
-	if [ -f "${1:-}" ]; then
-		grep ${2:-} ${1:-} | sed "s|.*${2:-}||" | sed 's|\"||g'
+	if [ -f "$1" ]; then
+		grep $2 $1 | sed "s|.*$2||" | sed 's|\"||g'
 	fi
 }
 
-get_ver() { sed -n 's/^date=//p' ${1:-}; }
+get_ver() { sed -n 's/^date=//p' $1; }
 
-print() { sed -n "s|^${1:-}=||p" $srcDir/module.prop; }
+print() { sed -n "s|^$1=||p" $srcDir/module.prop; }
 
 updater() {
-instVer=$(get_ver /data/adb/modules/$MODID/module.prop || :) 2>/dev/null
+instVer=$(get_ver /data/adb/modules/$MODID/module.prop) 2>/dev/null
 currVer=$(wget https://raw.githubusercontent.com/johnfawkes/$MODID/master/module.prop --output-document - | get_ver)
 zip=https://gitreleases.dev/gh/johnfawkes/fontchanger/latest/Fontchanger-$currVer.zip
 if [ $currVer -gt $instVer ]; then
-trap - EXIT
   wget --no-check-certificate -q -O $FCDIR/updates/Fontchanger-$currVer.zip $zip
   mkdir -p Fontchanger-$currVer
   unzip -o "Fontchanger-$currVer.zip" -d $FCDIR/updates/Fontchanger-$currVer >&2
@@ -83,7 +81,7 @@ trap - EXIT
   [ -d $installDir ] || { echo "(!) $installDir not found\n"; exit 1; }
 
   cat << CAT
-  $name $version $date
+  $name $version:$versionCode $date
   Copyright (c) 2019, $author
   License: GPLv3+
 
@@ -121,9 +119,7 @@ CAT
   $installDir/curl -k -o /storage/emulated/0/Fontchanger/fonts-list.txt https://john-fawkes.com/Downloads/fontlist/fonts-list.txt
   $installDir/curl -k -o /storage/emulated/0/Fontchanger/emojis-list.txt https://john-fawkes.com/Downloads/emojilist/emojis-list.txt
 
-  if [ $instVer == $currVer ]; then
-    echo "[!] Update Applied Successfully [!]"
-  fi
+  echo "[!] Update Applied Successfully [!]"
   exit 0
 else
   echo "[!] No Update Available [!]"
